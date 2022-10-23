04680eb65352c3246bf2776b52bb189e295a5b2e
Barkeal Beyene Gutema <barkealbeyene@gmail.com>
2022-10-22 20:08:10 -0400

    RPG Amended

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -4,77 +4,42 @@ let pName = input.question('What is your name? ');
 
 // Greeting and Define Player
 
-let player = {
-    name: pName,
-    hp: 75
+function MainPlayer(player, hp){
+    this.player = player;
+    this.hp = hp
 }
 
+let player = new MainPlayer (pName, 100)
+    
+
 let gameOver = false; 
-console.log('Welcome Gamer',player.name); 
+
+console.log('Welcome Gamer', pName); 
 
 //Define Enemy
 
-let wildEnemies = {name: 'Jaro', hp: 55}
+function enemies(enemyName, hp){
+    this.enemyName = enemyName;
+    this.hp = hp
+}
+
+let enemy = []
+    let enemy1 = new enemies('Harod', 60);
+    let enemy2 = new enemies('Largo', 70);
+    let enemy3 = new enemies('Nama', 80);
+
+enemy.push(enemy1, enemy2, enemy3)
+
+let randomEnemy = enemy[Math.floor(Math.random() * enemy.length)]
+
 
 //Define attacks 
 
 let playerChoice = input.keyIn('Do you want to walk or end game? ' + '(w) Walk or (e) End Game ', {limit: '$<w,e>;'});
 
 
-
-// startGame();
-
-// Define Walk
-// function walk(){
-//     let chance = Math.floor((Math.random() *2 ) + 1);
-//     if (chance == 1){
-//     let meetChance = Math.floor((Math.random() *2) +1);
-//     if(meetChance == 1){
-//         let meetEnemy =  input.keyIn('Oh, it seems that ' + wildEnemies.name + ' has found you' + ' (a) Attack or (r) Run away ', {limit: '$<a,r>;'})
-//         if(meetEnemy === 'a'){
-//             while( player.hp > 0 && wildEnemies.hp > 0){
-//                 let playerDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-//                 let enemyDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-//                 playerScore = " attacks dealing " + enemyDamage + " points of damage to you " ; player.hp = player.hp - enemyDamage
-//                 enemyScore = " attack " + wildEnemies.name + " and deal "  + playerDamage+ " points of damage, \n" ; wildEnemies.hp = wildEnemies.hp - playerDamage
-//                 playerScore = playerScore - Math.floor((Math.random() * 10) + 1);
-//             }
-//         } else if (meetEnemy === 'r'){
-//         runAway()
-//         }
-//     } else if(chance == 2){
-//         safe()
-//     }
-// }
-// }
-
-// function encounter(){
-//  let playerChoice = input.keyIn('Do you want to walk or Print stats? ' + '(w) Walk or (p) Print Stats ', {limit: '$<w,p>;'})
-//    if (playerChoice === 'w'){
-//     let chance = Math.floor((Math.random() *2) +1);
-//     if(chance == 1){
-//         let meetEnemy =  input.keyIn('Oh, it seems that ' + wildEnemies.name + ' has found you' + ' (a) Attack or (r) Run away ', {limit: '$<a,r>;'})
-//         if(meetEnemy === 'a'){
-//             while( player.hp > 0 && wildEnemies.hp > 0){
-//                 let playerDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-//                 let enemyDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-//                 playerScore = " attacks dealing " + enemyDamage + " points of damage to you " ; player.hp = player.hp - enemyDamage
-//                 enemyScore = " attack " + wildEnemies.name + " and deal "  + playerDamage+ " points of damage, \n" ; wildEnemies.hp = wildEnemies.hp - playerDamage
-//                 playerScore = playerScore - Math.floor((Math.random() * 10) + 1);
-//             }
-//         } else if (meetEnemy === 'r'){
-//         runAway()
-//         }
-//     } else if (chance == 2){
-//             runAway()
-//     } 
-//    } else if (playerChoice === 'p'){
-//     printStat()
-//    }
-// }
-
 function printStat(){
-    input.keyInPause('Hi ' + player.name + ' you have got ' + player.hp + ' health points left')
+    input.keyInPause('Hi ' + pName + ' you have got ' + player.hp + ' health points left')
     let playerChoice = input.keyIn('Do you want to walk or end game? ' + '(w) Walk or (e) End Game ', {limit: '$<w,e>;'});
     if(playerChoice == 'w'){
         walk()
@@ -90,83 +55,38 @@ function safe(){
     }
 }
 
-function loss(){
-    console.log('You have lost the game')
+
+function attack(){
+    let playerHP = player.hp;
+    let enemyHP = randomEnemy.hp
+    
+    while (playerHP > 0 && enemyHP > 0){
+                let playerDamage = Math.floor(Math.random() * 50);
+                console.log(playerDamage);
+                let enemyDamage = Math.floor(Math.random() * 100);
+                console.log(enemyDamage);
+                playerHP =  playerHP - playerDamage
+                enemyHP = enemyHP - enemyDamage;
+                console.log(playerHP, enemyHP) 
+            } 
+            if (playerHP > 0 && enemyHP < 0){
+                console.log('You have won')
+                let endChoice = input.keyIn('Do you want to check your health points or end the game? ' + '(p) Print Stat (e) End Game' , {limit:'$<p,e>;'})
+                if (endChoice == 'p'){
+                    printStat(pName)
+                } else {
+                    return gameOver = true; 
+                }
+            } else if (playerHP < 0 && enemyHP > 0){
+                console.log('The enemy has won')
+            } 
+            
+}
+            } 
 }
 
-// Game Controller 
-// if (player.hp > 0 && playerChoice == 'w'){
-//     walk()
-// } else if (player.hp > 0 && playerChoice == 'e'){
-//     gameOver = true;
-// } else if (player.hp <= 0){
-//     loss()
-//     gameOver = true; 
-// }
-
-// function runAway(){
-//     let runChance = Math.floor((Math.random() *2) +1);
-//         if(runChance == 1){
-//             let runAwayHit = Math.floor((Math.random() * 20) + 1);
-//             player.hp = player.hp - runAwayHit
-//             console.log('You did not successfully run away so you have taken some damage to your health points')
-//             encounter() 
-//         }
-//         else { 
-//             console.log('You have successfully run away and did not incure any damage to your health points')
-//             encounter()
-//         }
-// }
-
-// Define Attack 
-
-// function attack(){
-
-//     // create a while loop, start while loop here
-//     while( player.hp > 0 && wildEnemies.hp > 0){
-//         let playerDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-//         let enemyDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-//         playerScore = " attacks dealing " + enemyDamage + " points of damage to you " ; player.hp = player.hp - enemyDamage
-//         enemyScore = " attack " + wildEnemies.name + " and deal "  + playerDamage+ " points of damage, \n" ; wildEnemies.hp = wildEnemies.hp - playerDamage
-//         playerScore = playerScore - Math.floor((Math.random() * 10) + 1);
-//     }
-
-//     if (player.hp <= 0){
-//         console.log('You have died!')
-//         gameOver = true;
-//     } else if (wildEnemies.hp <=0 && player.hp > 0){
-//         console.log('You have defeated the Enemy!')
-//         encounter()
-//     }
-// }
-// Fight with Enemy 
-// function fight(){
-//     let meetEnemy =  input.keyIn('Oh, it seems that ' + wildEnemies.name + 'has found you' + ' (a) Attack or (r) Run away ', {limit: '$<a,r>;'})
-//     if(meetEnemy === 'a'){
-//         attack()
-//     } else if (meetEnemy === 'r'){
-//         runAway()
-//     }
-// }
-
-function walk(){
-    let chance = Math.floor((Math.random() *2 ) + 1);
-    if (chance == 1){
-    let meetChance = Math.floor((Math.random() *2) +1);
-    if(meetChance == 1){
-        let gameOver = false;
-        while (player.hp > 0 && wildEnemies.hp > 0){
-                let playerDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-                let enemyDamage = Math.floor(Math.random() * (100 - 35 + 10) + 5);
-                // playerScore = " attacks dealing " + enemyDamage + " points of damage to you " ; player.hp = player.hp - enemyDamage
-                playerScore = player.hp - playerDamage
-                // enemyScore = " attack " + wildEnemies.name + " and deal "  + playerDamage+ " points of damage, \n" ; wildEnemies.hp = wildEnemies.hp - playerDamage
-                enemyScore = wildEnemies.hp - enemyDamage;
-                console.log(playerScore)
-                gameOver = true; 
-            } 
-    } else if (meetChance == 2){
-        let runChance = Math.floor((Math.random() *2) +1);
+function runAway(){
+    let runChance = Math.floor((Math.random() *2) +1);
             if(runChance == 1){
                 let runAwayHit = Math.floor((Math.random() * 20) + 1);
                 player.hp = player.hp - runAwayHit
@@ -177,22 +97,21 @@ function walk(){
                 console.log('You have successfully run away and did not incure any damage to your health points')
                 walk()
             }
+}
+
+function walk(){
+    let chance = Math.floor((Math.random() * 2 ) + 1);
+    if (chance == 1){
+        let encounter = input.keyIn('Oops...You have encountered an enemy. What do you want to do? ' + '(a) Attack or (r) Run Away ', {limit: '$<a,r>;'})
+    // let meetChance = Math.floor((Math.random() *2) +1);
+    if(encounter == 'a'){
+        attack(randomEnemy)
+    } else if (encounter == 'r'){
+        runAway()
     }
 } else if(chance == 2){
     safe()
 }
-}
-
-
-// Game Controller 
-// if (player.hp > 0 && playerChoice == 'w'){
-//     walk()
-// } else if (player.hp > 0 && playerChoice == 'e'){
-//     gameOver = true;
-// } 
-if (player.hp <= 0){
-    loss()
-    gameOver = true; 
 }
 
 function startGame(){
@@ -210,9 +129,6 @@ function startGame(){
 
 
 startGame();
-
-
-
 
 
 
